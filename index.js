cbb6331e0daec932fd7d69d2b684c33c76844ca1
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-05-25 18:18:34 +0200

    added city name

--- index.js
+++ index.js
@@ -5,7 +5,17 @@
 // https://open-meteo.com/
 
 // 1. показывал температуру и расшифрованный код
+
 // 2. были стили шрифты картинки 
 // 3. деплой на gitPages
 // 4. Readme c описанием проекта и ссылкой на pages
+const cityName = document.getElementById("city");
 
+async function getIp(){
+    const responseObj = await fetch("https://get.geojs.io/v1/ip/geo.json");
+    const data = await responseObj.json();
+    const {latitude, longitude, city} = data;
+    cityName.textContent = city;
+}
+
+getIp();
